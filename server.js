57475bd2f729aca6d28db5dc1bf553e2db9f6429
Kelsee Watts <123975076+chillgatez@users.noreply.github.com>
2023-05-30 15:16:56 -0500

    lab15 starter

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 //Modularize your code by putting your schema and model in its own separate file and requiring the schema into your server.
 const Book = require('./book');
+const verifyUser = require('./authorize'); // Import the provided authorization middleware
 const app = express();
 
 app.use(cors());
 
 //for parsing json data
 app.use(express.json());
+
+// Apply the verifyUser middleware to all routes related to books
+app.use('/books', verifyUser);
 
 let DATABASE_URL = 'mongodb://localhost:27017/301'
 
@@ -28,7 +32,8 @@ const PORT = process.env.PORT || 3001;
 app.get('/books', async (request, response) => {
   try {
     await mongoose.connect(DATABASE_URL);
-    const books = await Book.find();
+    const userEmail = request.user.email; // access user's email from the request
+    const books = await Book.find({userEmail}); //find books by user
     response.json(books);
   } catch (error) {
     response.status(500).json({ error: 'books not found' });
@@ -40,7 +45,8 @@ app.post('/books', async (request, response) => {
   try {
     await mongoose.connect(DATABASE_URL);
     const { title, description, status } = request.body;
-    const newBook = await Book.create({ title, description, status });
+    const userEmail = request.user.email // acces user's email from request
+    const newBook = await Book.create({ title, description, status, userEmail });
     response.status(201).json(newBook);
   } catch {
     response.status(500).json({ error: 'failed to create new book' })
@@ -54,10 +60,10 @@ app.delete('/books/:id', async (request, response) => {
     await mongoose.connect(DATABASE_URL);
     //finds object by mongoose assigned id data type (objectId)
     const id = request.params.id;
-
+    const userEmail = request.user.email // access user's email from request
     //const bookID = mongoose.Types.ObjectId(id)
     console.log("book Id", id);
-    const deletedBook = await Book.findByIdAndDelete(id);
+    const deletedBook = await Book.findByIdAndDelete({_id: id, userEmail});
     if (deletedBook) {
       response.json({ message: 'book has been deleted' });
     } else {
@@ -71,9 +77,10 @@ app.delete('/books/:id', async (request, response) => {
 app.put('/books/:id', async (request, response) => {
   try {
     await mongoose.connect(DATABASE_URL);
+    const userEmail = request.user.email 
     const id = request.params.id;
     const { title, description, status } = request.body;
-    const updatedBook = await Book.findByIdAndUpdate(id, { title, description, status }, {new: true});
+    const updatedBook = await Book.findByIdAndUpdate({_id: id, userEmail}, { title, description, status }, {new: true});
 
     if (updatedBook) {
       response.status(201).json({message: 'Book successfully updated.'});
